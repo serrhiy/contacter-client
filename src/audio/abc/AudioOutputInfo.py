51a7aa327b0fc70bd3ba49279f1f32f8c84c4e64
Lytvynenko Serhii <serrheylitvinenko@gmail.com>
2025-04-30 23:01:25 +0300

    Add descriptioon for output audio device interface
    All implementations must necessarily implement this
    interface, and users must rely only on the methods
    defined here

---
+++ src/audio/abc/AudioOutputInfo.py
@@ -0,0 +1,8 @@
+"""
+This file defines api interface to retrieve
+information about output audio device.
+
+All implementations must necessarily implement this
+interface, and users must rely only on the methods
+defined here.
+"""
