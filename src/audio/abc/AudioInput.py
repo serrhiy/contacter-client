543d97ae47d85d9933e8992efd864572efc744cb
Lytvynenko Serhii <serrheylitvinenko@gmail.com>
2025-04-30 22:47:53 +0300

    Add description for audio input interface file
    All implementations must necessarily implement this
    interface, and users must rely only on the methods
    defined in it.

---
+++ src/audio/abc/AudioInput.py
@@ -0,0 +1,7 @@
+"""
+This file defines interface for audio input devices.
+
+All implementations must necessarily implement this
+interface, and users must rely only on the methods
+defined here.
+"""
