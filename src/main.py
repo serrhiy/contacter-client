131cda905e6009fdc8141c5a12dc51b5f8057087
Lytvynenko Serhii <serrheylitvinenko@gmail.com>
2025-05-01 18:47:33 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,13 +3,13 @@ import importlib
 from audio import abc
 from Config import Config
 from audio.abc.EngineInfo import FormatType
+import asyncio
 
 config_path = os.path.join(os.path.dirname(__file__), 'config.json')
 
-RECORD_SECONDS = 5
 CHUNK = 1024
 
-def main():
+async def main():
   config = Config.file(config_path)
   module: abc = importlib.import_module(f'audio.{config.implementation}')
   engine_info = module.EngineInfo.get_engine_info()
@@ -19,11 +19,12 @@ def main():
     idi = driver.get_default_input_device_info().index
     odi = driver.get_default_output_device_info().index
     rate = driver.get_default_input_device_info().default_sample_rate
-    input_stream = driver.open_input_stream(rate, 2, FormatType.int16, idi, CHUNK)
-    output_stream = driver.open_output_stream(rate, 2, FormatType.int16, odi, CHUNK)
-    for _ in range(0, int(rate / CHUNK * RECORD_SECONDS)):
-      output_stream.write(input_stream.read(CHUNK))
+    input_stream = driver.open_input_async_stream(rate, 2, FormatType.int16, idi, CHUNK)
+    output_stream = driver.open_output_async_stream(rate, 2, FormatType.int16, odi, CHUNK)
+    async for chunk in input_stream:
+      output_stream.write(chunk)
     input_stream.close()
     output_stream.close()
 
-if __name__ == '__main__': main()
+if __name__ == '__main__':
+  asyncio.run(main())
